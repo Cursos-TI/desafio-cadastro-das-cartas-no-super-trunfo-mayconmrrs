77e0b9c98afd39db9e81fb850b703e4082fafcbb
mayconmrrs <mayconmrrs@gmail.com>
2025-02-17 22:43:16 +0000

    inserido codigos para carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -30,14 +30,62 @@ int main() {
     printf("Digite o PIB:\n");
     scanf("%f", &PIB);
 
-    printf("Carta cadastarada com sucesso!\n");
+    printf("Carta 1 cadastarada com sucesso!\n");
+
     printf("Estado: %c\n", Estado);
+
     printf("Codigo da carta é: %c%s\n", Estado, Codigo);
+
     printf("Cidade: %s\n", Cidade);
+
     printf("Populacao: %d\n", Populacao);
+
     printf("Pontos Turisticos: %d\n", N_Pontos_Turisticos);
+
     printf("Area: %f KM²\n", Area);
+
+    printf("PIB: %f bilhoes de reais\n\n", PIB);
+
+    printf("Cadastre uma nova Carta\n\n");
+
+    printf(" Digite a inicial do Estado, de (A-H):\n");
+    scanf(" %c", &Estado);
+
+    printf("O Codigo da carta (01 a 04):\n");
+    scanf("%s", &Codigo);
+
+    printf("A Cidade:\n");
+    scanf("%s", &Cidade);
+
+    printf("A População:\n");
+    scanf("%d", &Populacao);
+
+    printf("Digite o numero de Pontos Turisticos:\n");
+    scanf("%d", &N_Pontos_Turisticos);
+
+    printf("Digite a Area:\n");
+    scanf("%f", &Area);
+
+    printf("Digite o PIB:\n");
+    scanf("%f", &PIB);
+
+    printf("Carta 2 cadastarada com sucesso!\n");
+
+    printf("Estado: %c\n", Estado);
+
+    printf("Codigo da carta é: %c%s\n", Estado, Codigo);
+
+    printf("Cidade: %s\n", Cidade);
+
+    printf("Populacao: %d\n", Populacao);
+
+    printf("Pontos Turisticos: %d\n", N_Pontos_Turisticos);
+
+    printf("Area: %f KM²\n", Area);
+
     printf("PIB: %f bilhoes de reais\n", PIB);
+
+    
 
 
 
