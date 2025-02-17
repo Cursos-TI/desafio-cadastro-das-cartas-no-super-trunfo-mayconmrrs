6498c1080c0380cba9592ea0434aba2f73dd92ce
mayconmrrs <mayconmrrs@gmail.com>
2025-02-17 23:25:47 +0000

    inseridos comentarios

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    char Codigo[4], Cidade[50];
-    char Estado;
-    int  Populacao, N_Pontos_Turisticos;
-    float Area, PIB;
+    char Codigo[4], Cidade[50]; //Definir Variavel do tipo caracteres
+    char Estado;                //Definir Variaiavel do tipo caracteres
+    int  Populacao, N_Pontos_Turisticos;  //Definir Variavel do tipo inteiro
+    float Area, PIB;                      //Defini Variavel do tipo flutuante
 
     printf("Desafio Super Trunfo - Paises\n");
     printf("Cadastro de Cartas\n\n");
     
-    printf("Digite a inicial do Estado, de (A-H):\n");
-    scanf("%c", &Estado);
+    printf("Digite a inicial do Estado, de (A-H):\n"); // Solicitação de entrada de dados para o usuario
+    scanf("%c", &Estado); // captura dos dados de entrada pelo usuario
 
-    printf("O Codigo da carta (01 a 04):\n");
+    printf("O Codigo da carta (01 a 04):\n"); 
     scanf("%s", &Codigo);
 
     printf("A Cidade:\n");
@@ -30,7 +30,7 @@ int main() {
     printf("Digite o PIB:\n");
     scanf("%f", &PIB);
 
-    printf("Carta 1 cadastarada com sucesso!\n");
+    printf("Carta 1 cadastarada com sucesso!\n"); //Imprimindo dados cadastrados
 
     printf("Estado: %c\n", Estado);
 
@@ -46,10 +46,10 @@ int main() {
 
     printf("PIB: %f bilhoes de reais\n\n", PIB);
 
-    printf("Cadastre uma nova Carta\n\n");
+    printf("Cadastre uma nova Carta\n\n"); //Solicitando novos dados de entrada
 
-    printf(" Digite a inicial do Estado, de (A-H):\n");
-    scanf(" %c", &Estado);
+    printf(" Digite a inicial do Estado, de (A-H):\n"); //inicio do novo cadastro
+    scanf(" %c", &Estado);  // Captura dos dados de entrada cadastrados pelo usuario
 
     printf("O Codigo da carta (01 a 04):\n");
     scanf("%s", &Codigo);
@@ -69,7 +69,7 @@ int main() {
     printf("Digite o PIB:\n");
     scanf("%f", &PIB);
 
-    printf("Carta 2 cadastarada com sucesso!\n");
+    printf("Carta 2 cadastarada com sucesso!\n"); // Imprimindo novos dados cadastrados
 
     printf("Estado: %c\n", Estado);
 
